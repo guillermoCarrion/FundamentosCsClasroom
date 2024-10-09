f1844b2f68cb199c3477fef3f470efc03ee828cc
guillermoCarrion <a27306@svalero.com>
2024-10-09 16:28:53 +0200

    ejercicio1

--- Program.cs
+++ Program.cs
@@ -4,30 +4,24 @@ ProgramaEducativo programa = new ProgramaEducativo();
 
 // Crear asignaturas
 Asignatura servidor = new Asignatura("Servidor", 6);
-var cliente = new Asignatura("Cliente", 4);
-Asignatura diseño = new("Diseño", 8);
 
 // Crear estudiantes
-var estudiante1 = new Estudiante("Vanessa Llorente");
-Estudiante estudiante2 = new Estudiante("Alejandro Giménez");
+Estudiante estudiante1 = new ("Guillermo");
 
 // Añadir estudiantes al programa educativo
 programa.AñadirEstudiante(estudiante1);
-programa.AñadirEstudiante(estudiante2);
 
 // Asignar calificaciones
 estudiante1.AñadirCalificacion(servidor, 9.5);
-estudiante1.AñadirCalificacion(cliente, 8.0);
-estudiante1.AñadirCalificacion(diseño, 9.0);
-
-estudiante2.AñadirCalificacion(servidor, 7.5);
-estudiante2.AñadirCalificacion(cliente, 8.5);
 
 // Mostrar estudiantes
 programa.MostrarEstudiantes();
 
+//Modificar calificacion
+estudiante1.ModificarCalificacion(servidor, 9.8);
+
 // Mostrar calificaciones de un estudiante específico
-Estudiante estudianteSeleccionado = programa.ObtenerEstudiante("Vanessa Llorente");
+Estudiante estudianteSeleccionado = programa.ObtenerEstudiante("Guillermo");
 if (estudianteSeleccionado != null)
 {
     estudianteSeleccionado.MostrarCalificaciones();
@@ -35,11 +29,3 @@ if (estudianteSeleccionado != null)
     Console.WriteLine($"Promedio de {estudianteSeleccionado.Nombre}: {promedio:F2}");
 }
 
-// Mostrar calificaciones del segundo estudiante
-estudianteSeleccionado = programa.ObtenerEstudiante("Alejandro Giménez");
-if (estudianteSeleccionado != null)
-{
-    estudianteSeleccionado.MostrarCalificaciones();
-    double promedio = estudianteSeleccionado.CalcularPromedio();
-    Console.WriteLine($"Promedio de {estudianteSeleccionado.Nombre}: {promedio:F2}");
-}
